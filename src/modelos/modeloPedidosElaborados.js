6a442edc97aa8b0428f3a85f4e0cd47c3167c046
Oscariniux <osanvago28@gmail.com>
2022-06-22 18:57:43 -0600

    se creo el modelo pedidos elaborados

--- src/modelos/modeloPedidosElaborados.js
+++ src/modelos/modeloPedidosElaborados.js
@@ -1,1 +1,33 @@
-probanding
+const {DataTypes}=require('sequelize')
+const db = require('../configuraciones/db')
+
+
+const pedidos_elaborados = db.define(
+    'pedidos_elaborados',
+    {
+        iddetallepedido: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        idusuario: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        fechahora: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        }
+    },
+    {
+        tableName:'pedidos_elaborados',
+        timestamps: false
+    }
+);
+
+pedidos_elaborados.sync().then(
+    () => console.log("Sincronizacion Completa")
+);
+
+module.exports = pedidos_elaborados;
