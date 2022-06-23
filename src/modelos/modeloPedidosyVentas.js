4f05c515499ca751d9dd649d5f3a02655f8bf081
Allanbre <allanbreve1999@gmail.com>
2022-06-23 10:57:27 -0600

    Finalizando crud

---
+++ src/modelos/modeloPedidosyVentas.js
@@ -0,0 +1,27 @@
+const {DataTypes} =require('sequelize')
+const db = require('../configuraciones/db')
+
+
+const PedidosyVentas = db.define(
+      'PedidosyVentas', 
+      {NumeroFactura:
+            {
+                  type: DataTypes.INTEGER,
+                  allowNull: false
+
+         },
+         NumeroPedido:
+         {
+            type: DataTypes.INTEGER,
+            allowNull: false
+         }
+      },
+      {
+            tableName: 'PedidosyVentas',
+            timestamps: false
+        }
+);
+PedidosyVentas.sync().then(
+      () => console.log("Sincronizacion Completa")
+    );
+    module.exports = PedidosyVentas;
