5a7d8c8cb47bacdb2969f11a1b294e6295e8b259
Francis-Lara <francislara247@gmail.com>
2022-06-26 03:54:14 -0600

    creacion del controlador de pedidos cancelados

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const app = express();
 app.set('port',3001);
 app.use(morgan('dev'));
+app.use(express.json());
 app.listen(app.get('port'), () => {
     console.log("Servidor inciado en el puerto " + app.get('port'));
 });
