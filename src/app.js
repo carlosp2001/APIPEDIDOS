f9ab24b7dec0c25a34fdeadcc743d76b936bbd95
carlosp2001 <85173449+carlosp2001@users.noreply.github.com>
2022-06-28 12:06:57 -0600

    Revert "Merge branch 'master' into pedidos"
    This reverts commit 8df57358245bb4235a46cea04b40f803ac3c3077.

--- src/app.js
+++ src/app.js
@@ -2,18 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 require('dotenv').config();
 const app = express();
-app.set('port', 3001);
+app.set('port',3001);
 app.use(morgan('dev'));
 app.use(express.json());
-
 app.use('/api/pedidos',require('./rutas/rutasPedidos'));
-app.use('/api/pedidosLlevar', require('./rutas/rutasPedidosLlevar'));
-app.use('/api/detallepedidos',require('./rutas/rutasDetallePedido'));
-app.use('/api/pedidosyVentas', require('./rutas/rutasPedidosyVentas'));
-app.use('/api/pedidoselaborados',require('./rutas/rutaspedidoselaborados'));
-app.use('/api/pedidosmesa', require('./rutas/rutasPedidosMesa'));
-app.use('/api/pedidosCancelados', require('./rutas/rutasPedidosCancelados'));
-
 app.listen(app.get('port'), () => {
     console.log("Servidor inciado en el puerto " + app.get('port'));
 });
