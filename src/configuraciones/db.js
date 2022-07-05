75e1d9b2167ed817f16dbaffd1da1421a1b13ca2
Caste <www.jrctabora@gmail.com>
2022-07-05 17:53:50 -0600

    Timeout Db

--- src/configuraciones/db.js
+++ src/configuraciones/db.js
@@ -7,7 +7,20 @@ const db = new sequelize(
         host: process.env.DB_HOST,
         dialect: process.env.DB_CONNECTION,
         port: process.env.DB_PORT,
+        "connectionTimeout": 0,
+        "pool":{
+            "max": 1,
+            "min": 1,
+            "idle": 200000,
+            "acquire": 200000
+        },
+        "retry":{"max": 3},
+        "dialectOptions": {
+            "requestTimeout": 0,
+            "connectTimeout": 0,
+        },
     }
+    
 );
 
 module.exports = db;  
