6d94fd32b38e8db9f122679f64f33252f534718b
Caste <www.jrctabora@gmail.com>
2022-07-05 18:03:38 -0600

    Fix Conexion

--- src/configuraciones/db.js
+++ src/configuraciones/db.js
@@ -1,5 +1,8 @@
 const sequelize = require('sequelize');
 const db = new sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
         dialect: process.env.DB_CONNECTION,
@@ -19,6 +22,5 @@ const db = new sequelize(
     }
     
 );
-
 
 module.exports = db;  
